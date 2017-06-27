2d0ab7aa2317fbcc77beaf6f7120c87014fc8f35
Shikha <shikhaberamolla@gmail.com>
2017-06-27 18:35:46 +0530

    initial

---
+++ kruskals.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+struct edge{ int src,dst,weight;
+               };
+int belong[20];
+int findset(int n)
+{
+    return(belong[n]);
+}
+void unionv(int a,int b,int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(belong[i]==b)
+            belong[i]=a;
+    }
+}
+void sortedges(struct edge graph[],int n)
+{
+    int i,j;
+    struct edge tmp;
+    for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+           if((graph[i].weight)>(graph[j].weight))
+          {
+               tmp=graph[i];
+               graph[i]=graph[j];
+               graph[j]=tmp;
+          }
+       }
+   }
+}
+void kruskals(struct edge graph[],int n)
+{
+    int minweight=0,i,v1,v2;
+    for(i=0;i<n;i++)
+    {   v1=findset(graph[i].src);
+        v2=findset(graph[i].dst);
+        if(v1!=v2)
+        {
+            //printf("weight included %d\n",graph[i].weight);
+            minweight=minweight+graph[i].weight;
+            unionv(v1,v2,n);
+
+        }
+
+    }
+    printf("\n the minimum weight is %d\n",minweight);
+}
+int main()
+{
+    struct edge graph[20];
+    int n,i;
+    printf("enter the total number of edges\n");
+    scanf("%d",&n);
+    printf("enter the nodes and weight of the edges\n");
+    for(i=0;i<n;i++)
+    {   belong[i]=i;
+        scanf("%d%d%d",&graph[i].src,&graph[i].dst,&graph[i].weight);
+    }
+    sortedges(graph,n);
+    kruskals(graph,n);
+    return 0;
+}
